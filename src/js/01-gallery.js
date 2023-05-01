0ab32ba73188635833446ef5e87f3b974b45f89e
PiechBartosz <p.bartosz@yahoo.com>
2023-05-01 18:52:31 +0200

    upgrade

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,50 +1,34 @@
 // Add imports above this line
 
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-const gallery = document.querySelector(".gallery");
-galleryItems.forEach((item) => {
-	const li = document.createElement("li");
-	const link = document.createElement("a");
-	const img = document.createElement("img");
+const gallery = document.querySelector('.gallery');
 
-	li.classList.add("gallery__item");
-	link.classList.add("gallery__link");
-	img.classList.add("gallery__image");
+galleryItems.forEach(item => {
+  const li = document.createElement('li');
+  const link = document.createElement('a');
+  const img = document.createElement('img');
 
-	link.href = item.original;
-	img.src = item.preview;
-	img.alt = item.description;
-	img.setAttribute("data-source", item.original);
+  li.classList.add('gallery__item');
+  link.classList.add('gallery__link');
+  img.classList.add('gallery__image');
 
-	link.appendChild(img);
-	li.appendChild(link);
-	gallery.appendChild(li);
+  link.href = item.original;
+  img.src = item.preview;
+  img.alt = item.description;
+
+  link.appendChild(img);
+  li.appendChild(link);
+  gallery.appendChild(li);
 });
 
-// Open modal on click
-gallery.addEventListener("click", (e) => {
-	e.preventDefault();
-	if (e.target.classList.contains("gallery__image")) {
-		const modal = basicLightbox.create(
-			`<img src="${e.target.dataset.source}" alt="${e.target.alt}" />`,
-			{
-				onClose: (instance) => {
-					window.removeEventListener("keydown", onKeyDown);
-				},
-			}
-		);
-
-		const onKeyDown = (e) => {
-			if (e.code === "Escape") {
-				modal.close();
-			}
-		};
-
-		window.addEventListener("keydown", onKeyDown);
-		modal.show();
-	}
+const galleryItemsNodes = document.querySelectorAll('.gallery__link');
+new SimpleLightbox(galleryItemsNodes, {
+  captions: true,
+  captionDelay: 250,
 });
