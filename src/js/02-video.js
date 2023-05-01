0d020ae94c2b6b033096217e961c028a907390c3
PiechBartosz <p.bartosz@yahoo.com>
2023-05-01 18:22:54 +0200

    upgrade

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+let iframe = document.querySelector('#vimeo-player');
+let player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+player.ready().then(function () {
+  let savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime !== null) {
+    player.setCurrentTime(savedTime);
+  }
+});
